d3691203a8c3b71d47d2c97f13d8c547c8cb2a97
lynch <louis.lynch@evodev.ca>
2024-12-04 22:21:59 -0500

    fix: autocomplete selection with enter

--- config/nvim.scratch/after/plugin/lsp.lua
+++ config/nvim.scratch/after/plugin/lsp.lua
@@ -64,7 +64,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ['<C-y>'] = cmp.mapping.confirm({select = true}),
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
   snippet = {
